7d0fc59a70ebae597ba9030d9536eebb773d0193
EugenNaydyon <eugennaydyon@gmail.com>
2022-06-08 17:07:33 +0300

    tablet in process

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,13 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-//   const mobileBtnClose = document.querySelector("[data-menu-close]");
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
 
-//   menuBtnRef.addEventListener("click", () => {
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
 
-//   mobileBtnClose.addEventListener("click", () => {
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
-
-
-function checkPassword(password) {
-  const ADMIN_PASSWORD = "jqueryismyjam";
-  let message;
-  // Change code below this line
-  
-  switch (password) {
-  case null:
-  message = "Canceled by user!";
-  break;
-  
-  case ADMIN_PASSWORD:
-  message = "Welcome!";
-  break;
-      
-  default:
-  console.log("Access denied, wrong password!");
-  }
-  console.log(message);
-}
-
-checkPassword("mangohackzor")
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
